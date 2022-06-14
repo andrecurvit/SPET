ee992c1ae6706d3c14978dbc5b3c8de176ee17c6
andrecurvit <andrealmeida.unicid.1c@gmail.com>
2022-06-13 21:18:09 -0300

    atualização/correção

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# SPET
-PetShop Projeto iniciado
+# SPET-Hotel-para-Pets
+Neste projeto estamos fazendo um hotel para pets. Junto com Eduardo Shodi github: https://github.com/EduardoShodi
+
+Estamos trabalhando para que este projeto seja um dos melhores que já fizemos.
---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/style.css" rel="stylesheet">
+    
+    <title>SPET - O melhor hotel para seu pet</title>
+</head>
+<body>
+    <header> 
+        <nav>
+            <ul class="lista">
+                <figure>
+                    <img src="assets/img/logo.png" class="logo">
+                </figure>
+                <button><a href="#">Agende já!</a></button>
+                <li><a href="#reserva">Reserva</a></li>
+                <li><a href="#depoimentos">Depoimentos</a></li>
+                <li><a href="#sobre-nos">Sobre nós</a></li>
+                <li><a href="#nosso-espaco">Nosso espaço</a></li>
+                <li><a href="#home">Home</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div>
+        <section id="home">
+            <h1 class="texto-central">Home</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        </section>
+        <hr>
+        <section id="nosso-espaco">
+            <h1 class="texto-central">Nosso espaço</h1>
+            <br>
+            <div class="fotos-espaco">
+                <div class="fotos-espaco1">
+                    <img class="imagem1" src="assets/img/local1.jpg" alt="imagem do local">
+                    <p class="texto-imagem1">Aqui no O nosso espaço conta com 200m². Temos diversas baias e campos com áreas abertas verdes para 
+                        seu pet se sentir e ser como um cachorro (literalmente) e também áreas cobertas para se abrigarem das chuvas. <br><br>
+                        Nosso espaço é cercado de árvores, brinquedos como mordedores, bonecos de borracha para eles
+                        morderem aplicando a força real de suas bocas, tatames, casinhas, árvores para eles pulares (somente os de médio porte).
+                    </p>
+                </div>
+                <hr>
+                <br>
+                <div class="fotos-espaco2">
+                    <img class="imagem2" src="assets/img/local2.jpg" alt="imagem do local">
+                    <p class="texto-imagem2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, 
+                        laboriosam accusantium ut recusandae ipsa nemo optio molestiae minus sequi? Deleniti 
+                        neque animi similique commodi voluptate tenetur odit qui debitis quis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, 
+                        laboriosam accusantium ut recusandae ipsa nemo optio molestiae minus sequi? Deleniti 
+                        neque animi similique commodi voluptate tenetur odit qui debitis quis.
+                    </p>
+                </div>
+                <hr>
+                <br>
+                <div class="fotos-espaco3">
+                    <img class="imagem3" src="assets/img/local3.jpg" alt="imagem do local">
+                    <p class="texto-imagem3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, 
+                        laboriosam accusantium ut recusandae ipsa nemo optio molestiae minus sequi? Deleniti 
+                        neque animi similique commodi voluptate tenetur odit qui debitis quis. 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, 
+                        laboriosam accusantium ut recusandae ipsa nemo optio molestiae minus sequi? Deleniti 
+                        neque animi similique commodi voluptate tenetur odit qui debitis quis.
+                    </p>
+                </div>
+                <div class="qualquercoisa"> \    </div>
+            </div>
+
+        <hr>
+        <section id="sobre-nos">
+            <h1 class="texto-central">Sobre nós</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        </section>
+        <hr>
+        <section id="depoimentos">
+            <h1 class="texto-central">Depoimentos</h1>
+            
+            <div class="retangulo">
+                <div class="center">
+                    <div class="retangulo-single">
+                        <div class="retangulo-wraper">
+                            <div class="fundo-retangulo">
+                                <div class="foto-cao" alt="cachorro"><img src="assets/img/cachorro1.webp" class="cachorro"></img></div>
+                                <div class="depoimento-dono">
+                                    <div class="foto-dona"><img src="assets/img/pessoa3.webp" class="dona"></div><p class="nome-dono">Eduardo:</p>
+                                    <p class="comentario">
+                                        Um lugar lindo e espaçoso para os Pets. Estando no local percebi que cada cantinho
+                                    foi cuidadosamente planejado e construído. Os proprietários realmente tem amor aos
+                                    animais! Isso fica bem evidente. Parabéns! Recomendo!!</p>
+                                </div>
+                            </div><!--fundo-retangulo-->
+                        </div><!--retangulo-wraper-->
+                    </div><!--retangulo-single-->
+                          
+                    <div class="retangulo-single">
+                        <div class="retangulo-wraper">
+                        <div class="fundo-retangulo">
+                                <div class="foto-cao" alt="cachorro">
+                                    <img src="assets/img/cachorro3.webp" class="cachorro">
+                                </div>
+                                <div class="depoimento-dono">
+                                    <div class="foto-dona">
+                                        <img src="assets/img/pessoa3.webp" class="dona">
+                                    </div>
+                                    <p class="nome-dono">Eduardo:</p>
+                                    <p class="comentario">
+                                        Um lugar lindo e espaçoso para os Pets. Estando no local percebi que cada cantinho
+                                    foi cuidadosamente planejado e construído. Os proprietários realmente tem amor aos
+                                    animais! Isso fica bem evidente. Parabéns! Recomendo!!</p>
+                                </div>
+                            </div><!--fundo-retangulo-->
+                        </div><!--retangulo-wraper-->
+                    </div><!--retangulo-single-->
+                               
+                    <div class="retangulo-single">
+                        <div class="retangulo-wraper">
+                        <div class="fundo-retangulo">
+                                <div class="foto-cao" alt="cachorro">
+                                    <img src="assets/img/cachorro2.webp" class="cachorro">
+                                </div>
+                                <div class="depoimento-dono">
+                                    <div class="foto-dona">
+                                        <img src="assets/img/pessoa3.webp" class="dona">
+                                    </div>
+                                    <p class="nome-dono">Eduardo:</p>
+                                    <p class="comentario">
+                                        Um lugar lindo e espaçoso para os Pets. Estando no local percebi que cada cantinho
+                                    foi cuidadosamente planejado e construído. Os proprietários realmente tem amor aos
+                                    animais! Isso fica bem evidente. Parabéns! Recomendo!!</p>
+                                </div>
+                            </div><!--fundo-retangulo-->
+                        </div><!--retangulo-wraper-->
+                    </div><!--retangulo-single-->
+                </div><!--center-->
+            </div><!--retangulo-->
+            <br>
+        </section>
+        <hr>
+        <section id="reserva">
+            <h1 class="texto-central fazer-reserva">FAÇA SUA RESERVA</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <hr>
+       
+        </section>
+        <hr>
+       
+    </div>
+    <footer>
+        ©Copyright - <strong>Desenvolvedores</strong><br>
+        <a href="https://github.com/andrecurvit/"><img src="assets/img" alt="(Foto Andre Almeida)"> Andre Almeida</a><br> 
+        <a href="https://github.com/EduardoShodi"><img src="" alt="(Foto Eduardo Sagaiama)"> Eduardo Sagaiama</a><br>
+        <p>Termos de serviço.</p>
+    </footer>
+</body>
+</html>
