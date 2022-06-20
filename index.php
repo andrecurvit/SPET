e41b1b1619a9178f9a091c566e2ff20b8f4958aa
andrecurvit <andrealmeida.unicid.1c@gmail.com>
2022-06-20 20:29:48 -0300

    main

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Teste</title>
+    <title>SPET - O melhor lugar para seus pets</title>
     <link rel="stylesheet" type="text/css" href="bootstrap5.2/css/bootstrap.min.css">
     <script type="text/javascript" src="bootstrap/bootstrap5.2/js/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="bootstrap5.2/js/bootstrap.min.js"></script>
@@ -12,21 +12,43 @@
     <body>
     <header>
     	<nav>
-    		<a class="logo" href="/">Teste</a>
+    		<a class="logo" href="assets/img/logo.png"><img src="assets/img/logo.png" class="logo" alt="Logo"></a>
     		<div class="mobile-menu">
     			<div class="line1"></div>
     			<div class="line2"></div>
     			<div class="line3"></div>
     		</div>
-    		<ul class="nav-list">
-    			<li><a href="/">Inicio</a></li>
-    			<li><a href="/">Sobre</a></li>
-    			<li><a href="/">Projeto</a></li>
-    			<li><a href="/">Contato</a></li>
-                <button class="agendar"><a href=""> AGENDE JÁ!</a></button>
+            <!-- <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul> -->
+
+    		
+    		<ul class="nav-lista">
+    			<li><a href="#">Home</a></li>
+    			<li><a href="#">Nosso espaço</a></li>
+    			<li><a href="#">Sobre nós</a></li>
+    			<li><a href="#">Depoimentos</a></li>
+                <li><a href="#"></a></li>
+                <!-- <a class="btn btn-info">Agende Já</a> -->
+                <li class="agendar"><a href="#"> Agende Já!</a></li>
     		</ul>
     	</nav>
     </header>
+    <section id="home">
+
+    </section>
+    <section></section>
     <section id="carrossel">
         <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
             <div class="carousel-indicators">
@@ -36,21 +58,21 @@
             </div>
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img src="../SPET/assets/img/logo.png" class="d-block w-100" alt="...">
+                <img src="assets/img/logo.png" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>First slide label</h5>
                   <p>Some representative placeholder content for the first slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="../SPET/assets/img/logo.png" class="d-block w-100" alt="...">
+                <img src="assets/img/logo.png" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Second slide label</h5>
                   <p>Some representative placeholder content for the second slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="../SPET/assets/img/logo.png" class="d-block w-100" alt="...">
+                <img src="assets/img/logo.png" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Third slide label</h5>
                   <p>Some representative placeholder content for the third slide.</p>
@@ -71,8 +93,8 @@
 
     <footer>
         ©Copyright - <strong>Desenvolvedores</strong><br>
-        <a href="https://github.com/andrecurvit/"><img src="assets/img" alt="(Foto Andre Almeida)"> Andre Almeida</a><br> 
-        <a href="https://github.com/EduardoShodi"><img src="" alt="(Foto Eduardo Sagaiama)"> Eduardo Sagaiama</a><br>
+        <a href="https://github.com/andrecurvit/"><img src="assets/img/Andre.png" alt="(Foto Andre Almeida)" class="imgdevs"> Andre Almeida</a><br> 
+        <a href="https://github.com/EduardoShodi"><img src="assets/img/Eduardo.png" alt="(Foto Eduardo Sagaiama)" class="imgdevs"> Eduardo Sagaiama</a><br>
         <p>Termos de serviço.</p>
     </footer>
 </body>
