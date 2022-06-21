d93d2f755e6feada4abaebbc01181616282ac028
andrecurvit <andrealmeida.unicid.1c@gmail.com>
2022-06-21 18:38:49 -0300

    Adicionado páginas home e nosso espaço

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon" />
     <title>SPET - O melhor lugar para seus pets</title>
     <link rel="stylesheet" type="text/css" href="bootstrap5.2/css/bootstrap.min.css">
     <script type="text/javascript" src="bootstrap/bootstrap5.2/js/jquery-3.6.0.min.js"></script>
@@ -12,82 +13,80 @@
     <body>
     <header>
     	<nav>
-    		<a class="logo" href="assets/img/logo.png"><img src="assets/img/logo.png" class="logo" alt="Logo"></a>
+    		<a class="logo" href="#"><img src="assets/img/logo.png" class="logo" alt="Logo"></a>
     		<div class="mobile-menu">
     			<div class="line1"></div>
     			<div class="line2"></div>
     			<div class="line3"></div>
-    		</div>
-            <!-- <ul class="nav nav-tabs">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul> -->
-
-    		
-    		<ul class="nav-lista">
-    			<li><a href="#">Home</a></li>
-    			<li><a href="#">Nosso espaço</a></li>
-    			<li><a href="#">Sobre nós</a></li>
-    			<li><a href="#">Depoimentos</a></li>
-                <li><a href="#"></a></li>
-                <!-- <a class="btn btn-info">Agende Já</a> -->
-                <li class="agendar"><a href="#"> Agende Já!</a></li>
+    		</div>    		
+    		<ul class="nav-list">
+    			<li class="btn btn-outline-primary"><a href="#home">Home</a></li>
+    			<li class="btn btn-outline-primary"><a href="#nosso-espaco">Nosso espaço</a></li>
+    			<li class="btn btn-outline-primary"><a href="#">Sobre nós</a></li>
+    			<li class="btn btn-outline-primary"><a href="#">Depoimentos</a></li>
+          <li class="btn btn-outline-primary"><a href="#">Contato</a></li>
+          <li class="agendar"><a href="#"> Agende Já!</a></li>
     		</ul>
     	</nav>
     </header>
     <section id="home">
-
-    </section>
-    <section></section>
-    <section id="carrossel">
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-            <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="assets/img/logo.png" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>First slide label</h5>
-                  <p>Some representative placeholder content for the first slide.</p>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/img/logo.png" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>Second slide label</h5>
-                  <p>Some representative placeholder content for the second slide.</p>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/img/logo.png" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>Third slide label</h5>
-                  <p>Some representative placeholder content for the third slide.</p>
-                </div>
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
+        <img src="assets/img/puppy.png" alt="" class="dog1">
+        <div class="card card-initial">
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            <h6 class="card-subtitle mb-2 text-muted">Seu pet precisa de um hotel que nem você para descansar!!!</h6>
+            <p class="card-text">Nossos serviços contam com diversos tipos de brincadeiras, treinamentos¹, gincanas e muito mais!!!!  .</p>
+            <a href="#" class="card-link">Reservar agora!</a>
+            <a href="#" class="card-link">Conhecer mais!</a>
+          </div>
         </div>
+    </section>
+    <section id="nosso-espaco">
+      <div class="nosso-espaco" style="display: inline-flex;">  
+        <h1>Um pouco mais do noss espaço</h1>
+        <div class="card" style="width: 400px; top: 5%;">
+          <img src="assets/img/local1.jpg" class="card-img-bottom" alt="..." style="width: 400px; height: 320px;">
+          <div class="card-body">
+            <p class="card-text" style="color: blue;">Nosso espaço consiste em diversas baias para os seus pets!</p>
+          </div>
+        </div>
+        <div class="card" style="width: 400px; top: 5%;">
+          <img src="assets/img/local1.jpg" class="card-img-bottom" alt="..." style="width: 400px; height: 320px;">
+          <div class="card-body">
+            <p class="card-text" style="color: blue;">Descrição 2</p>
+          </div>
+        </div>
+        <div class="card" style="width: 400px; top: 5%;">
+          <img src="assets/img/local1.jpg" class="card-img-bottom" alt="..." style="width: 400px; height: 320px;">
+          <div class="card-body">
+            <p class="card-text" style="color: blue;">Descrição 3</p>
+          </div>
+        </div>
+        <!-- <img src="assets/img/dog-espaco.png" alt="" class="dog-espaco"> -->
+      </div>
+    </section>
+    <section id="carrossel">
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/img/cachorro1.webp" class="d-block w-100" alt="..." style="height: 400px">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/cachorro2.webp" class="d-block w-100" alt="..." style="height: 400px">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/cachorro3.webp" class="d-block w-100" alt="..." style="height: 400px">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     </section>
 
 
