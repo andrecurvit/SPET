fab4bbb142284d28d9d79eec2d1ab61b1507dd59
EduardoShodi <sagayama15@gmail.com>
2022-06-22 20:43:20 -0300

    eu comentei la no css

--- index.php
+++ index.php
@@ -70,13 +70,16 @@
     <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="assets/img/cachorro1.webp" class="d-block w-100" alt="..." style="height: 400px">
+          <div class="quadrado"></div>
+          <img src="assets/img/cachorro1.webp" class="d-block imagemdog" alt="..." style="height: 250px">
         </div>
         <div class="carousel-item">
-          <img src="assets/img/cachorro2.webp" class="d-block w-100" alt="..." style="height: 400px">
+          <div class="quadrado"></div>
+          <img src="assets/img/cachorro2.webp" class="d-block imagemdog" alt="..." style="height: 250px">
         </div>
         <div class="carousel-item">
-          <img src="assets/img/cachorro3.webp" class="d-block w-100" alt="..." style="height: 400px">
+          <div class="quadrado"></div>
+          <img src="assets/img/cachorro3.webp" class="d-block imagemdog" alt="..." style="height: 250px">
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
